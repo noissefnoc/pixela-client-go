f03e710507870b0b776587546551b4a855cf4bc0
noissefnoc <noissefnoc@gmail.com>
2019-01-09 10:36:45 +0900

    add description at pixel.go

--- cmd/pixel.go
+++ cmd/pixel.go
@@ -1,23 +1,3 @@
-// Copyright © 2019 Kota Saito
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package cmd
 
 import (
@@ -29,14 +9,12 @@ import (
 // pixelCmd represents the pixel command
 var pixelCmd = &cobra.Command{
 	Use:   "pixel",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "handle pixel sub-command",
+	Long: `record, get, update, increment, decrement and delete pixel
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+see official document (https://docs.pixe.la) for more detail`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: change this statement print `pixela pixel` sub-commands
 		fmt.Println("pixel called")
 	},
 }
