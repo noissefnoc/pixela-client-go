e61c20f2d4e28da05fcf38a414a61b8f3858025c
Kota Saito <noissefnoc@gmail.com>
2019-01-10 19:26:24 +0900

    add feature: `user create` saves authenticate data to file

--- cmd/usercreate.go
+++ cmd/usercreate.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-homedir"
 	"github.com/noissefnoc/pixela-client-go/pixela"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 // usercreateCmd represents the usercreate command
@@ -19,14 +23,18 @@ see official document (https://docs.pixe.la/#/post-user) for more detail.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// check arguments
 		if len(args) != 2 {
-			fmt.Fprintf(os.Stderr, "argument error: `user create` requires 2 arguments give %d arguments.\n", len(args))
+			fmt.Fprintf(
+				os.Stderr,"argument error: `user create` requires 2 arguments give %d arguments.\n", len(args))
 			os.Exit(1)
 		}
 
+		username := args[0]
+		token := args[1]
+
 		// do request
 		client := pixela.Pixela{
-			Username: args[0],
-			Token:    args[1],
+			Username: username,
+			Token:    token,
 			Debug:    true,
 		}
 
@@ -35,6 +43,31 @@ see official document (https://docs.pixe.la/#/post-user) for more detail.`,
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "pixela request error:\n%v\n", err)
 			os.Exit(1)
+		}
+
+		configFilePath := viper.GetString("config")
+
+		if !existFile(configFilePath) && !existFile(getDefaultFilePath()) {
+			saveConfigFilePath := configFilePath
+
+			if saveConfigFilePath == "" {
+				saveConfigFilePath = getDefaultFilePath()
+			}
+
+			// TODO: apply yaml package
+			settings := fmt.Sprintf("username: %s\ntoken: %s\n", username, token)
+
+			err := ioutil.WriteFile(saveConfigFilePath, []byte(settings), 0644)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "write config error:\n%+v\n", err)
+				os.Exit(1)
+			}
+		} else {
+			fmt.Fprintf(os.Stderr, "`pixel create` successd but there are aleady config file at %s\n", configFilePath)
+			fmt.Fprintf(os.Stderr, "You should take a note following settings(save to yaml file).\n")
+			fmt.Fprintf(os.Stderr, "username: %s\n", username)
+			fmt.Fprintf(os.Stderr, "token: %s\n", token)
 		}
 	},
 }
@@ -51,4 +84,27 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// usercreateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// check if file exists
+func existFile(checkFilePath string) bool {
+	_, err := os.Stat(checkFilePath)
+
+	if err == nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+// default config file path
+func getDefaultFilePath() string {
+	homeDirPath, err := homedir.Dir()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return filepath.Join(homeDirPath, ".pixela.yaml")
 }
