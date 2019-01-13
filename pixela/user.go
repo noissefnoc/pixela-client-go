95a95b00389403e40cc8bd927ad290fda16f628a
noissefnoc <noissefnoc@gmail.com>
2019-01-13 20:19:12 +0900

    add `user update` subcommand.

--- pixela/user.go
+++ pixela/user.go
@@ -14,6 +14,10 @@ type CreateUserPayload struct {
 	NotMinor            string `json:"notMinor"`
 }
 
+type UpdateUserPayload struct {
+	NewToken string `json:"newToken"`
+}
+
 // create user
 func (pixela *Pixela) CreateUser() (NoneGetResponseBody, error) {
 	// create payload
@@ -46,6 +50,40 @@ func (pixela *Pixela) CreateUser() (NoneGetResponseBody, error) {
 
 	if err != nil {
 		return NoneGetResponseBody{}, errors.Wrap(err, "error `user create`:http response parse failed.")
+	}
+
+	return postResponseBody, nil
+}
+
+// update user token
+func (pixela *Pixela) UpdateUser(newToken string) (NoneGetResponseBody, error) {
+	// create payload
+	pl := UpdateUserPayload{
+		NewToken: newToken,
+	}
+
+	plJSON, err := json.Marshal(pl)
+
+	if err != nil {
+		return NoneGetResponseBody{}, errors.Wrap(err, "error `user create`: can not marshal request payload.")
+	}
+
+	// build request url
+	// TODO: rewrite by url package
+	requestURL := fmt.Sprintf("%s/v1/users/%s", baseUrl, pixela.Username)
+
+	// do request
+	responseBody, err := pixela.put(requestURL, bytes.NewBuffer(plJSON))
+
+	if err != nil {
+		return NoneGetResponseBody{}, errors.Wrap(err, "error `user update`:http request failed.")
+	}
+
+	postResponseBody := NoneGetResponseBody{}
+	err = json.Unmarshal(responseBody, &postResponseBody)
+
+	if err != nil {
+		return NoneGetResponseBody{}, errors.Wrap(err, "error `user update`:http response parse failed.")
 	}
 
 	return postResponseBody, nil
