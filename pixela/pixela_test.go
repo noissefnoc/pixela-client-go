a541f8e16780b7e62b7a4c3bfe941b0e89d22228
noissefnoc <noissefnoc@gmail.com>
2019-02-07 01:05:47 +0900

    add pixela.get unit test

--- pixela/pixela_test.go
+++ pixela/pixela_test.go
@@ -44,7 +44,7 @@ func TestNew(t *testing.T) {
 }
 
 // common values on following tests
-var url = "https://examples.com/"
+var urlStr = "https://examples.com/"
 var username = "testuser"
 var token = "testtoken"
 var debug = false
@@ -101,7 +101,52 @@ func TestPixela_post(t *testing.T) {
 				t.Fatalf("got error when http client created %#v", err)
 			}
 
-			_, err = pixela.post(url, tt.payload)
+			_, err = pixela.post(urlStr, tt.payload)
+
+			if err != nil {
+				if err.Error() != tt.wantErr.Error() {
+					t.Fatalf("want %#v, but %#v", tt.wantErr, err)
+				}
+			}
+		})
+	}
+}
+
+// test for pixela.get
+func TestPixela_get(t *testing.T) {
+	tests := []struct {
+		name       string
+		statusCode int
+		response   *bytes.Buffer
+		wantErr    error
+	}{
+		{"normal case", 200, bytes.NewBufferString("success"), nil},
+		{"response status not ok", 403, bytes.NewBufferString("error"), errors.New("returns none success status code: 403")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := &http.Response{
+				StatusCode: tt.statusCode,
+				Body:       ioutil.NopCloser(tt.response),
+				Header:     make(http.Header),
+			}
+
+			srClient := NewTestClient(func(req *http.Request) *http.Response {
+				if req.Header.Get("X-USER-TOKEN") != token {
+					t.Fatalf("want %#v, but %#v", token, req.Header.Get("X-USER-TOKEN"))
+				}
+
+				return resp
+			})
+
+			pixela, err := New(username, token, debug, OptionHTTPClient(srClient))
+
+			if err != nil {
+				t.Fatalf("got error when http client created %#v", err)
+			}
+
+			_, err = pixela.get(urlStr)
 
 			if err != nil {
 				if err.Error() != tt.wantErr.Error() {
